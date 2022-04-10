41031cbad94125bf95b91f9b9f372cd974852394
minghuiYu <1766743814@qq.com>
2022-04-10 18:13:18 +0800

    添加单例工具类，状态机基类，游戏总控制类

---
+++ Project/Assets/Scripts/GameCtrls/GameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// levelNames: Menu \level1\level2\level3\level4\level5
+/// 
+/// </summary>
+/// 
+public enum ELevel 
+{
+    Menu = 0,
+    Level1,
+    Level2,
+    Level3,
+    Level4,
+    Level5
+}
+
+public class GameController : MonoSingleton<GameController>
+{
+    public ELevel nowLevel = ELevel.Menu;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void ChangeScence(ELevel level)
+    {
+        Debug.Log("当前场景：" + level.ToString());
+        nowLevel = level;
+        SceneManager.LoadScene((int)level);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
